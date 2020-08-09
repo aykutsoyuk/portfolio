b4e1cade8ba55cdffede0c79615f23a4c103f5d6
“aykutsoyuk” <aykutsoyuk@gmail.com>
2020-08-09 19:09:07 +0300

    responsiveDesign

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
-   $('.knowledge-info').t({
-      speed:80,
-      delay:1
-   })
-   
-})
+$(document).ready(function () {
+   $(".hamburger").click(() => {
+      $(".nav-link").toggleClass("open");
+      $(".nav-link").hasClass("open")
+      ? $(".hamburger").css({ position: "fixed", top: "3%" })
+      : $(".hamburger").css({ position: "absolute", top: "50%" });
+   });
+   $(".button-list").click(() => {
+      $(".nav-link").removeClass("open");
+      $(".hamburger").css({ position: "absolute", top: "50%" });
+   });
+   $(window).width() < 450
+   ? $(".knowledge").empty()
+   .append(`<p class="knowledge-info">
+   <del style="text-decoration: none;"
+   >Hello. I'm Aykut... <ins>1</ins>
+   </del>
+   <del style="text-decoration: none;">I love coding & music...<ins>1</ins></del
+   >Here is my portfolio.
+   </p>`)
+   : false;
+   $(".knowledge-info").t({
+     speed: 80,
+     delay: 1,
+   });
+});
